f9bbd37dae19f0c71877b5ae8fcc3670d8933483
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2021-01-04 23:16:44 +0100

    Leaflet markers icon and tooltip as vue components

--- public/app.js
+++ public/app.js
@@ -1,81 +1,106 @@
+var placesVM = undefined;
 jQuery(document).ready(function($){
   $(".sidebar-control").on('click', _ => {
     document.getElementById("sidebar").classList.toggle("collapsed");
     document.getElementById("sidebar-header").classList.toggle("collapsed");
   });
 
-  function entry_icon(type) {
-      if (type == "M")
-          return "<div style='background-color:#0bc32b;' class='marker-pin'></div><i class='material-icons' style='color: rgb(0, 0, 0);'>house</i>";
-      if (type == "T3")
-          return "<div style='background-color:#c30b82;' class='marker-pin'></div><span style='color: rgb(0, 0, 0);'>T3</span>";
-      if (type == "T4")
-          return "<div style='background-color:#0b73c3;' class='marker-pin'></div><span style='color: rgb(0, 0, 0);'>T4</span>";
+  let popup = L.popup();
+  let map = L.map('map', {zoomControl: false}).setView([48.77199820642046, 2.0856456177490124], 15);
+
+  L.control.zoom({position:'topright'}).addTo(map);
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+  }).addTo(map);
+
+  map.on('click', function(e) { popup.setLatLng(e.latlng).setContent(e.latlng.toString()).openOn(this); });
+
+  function placeTitle(p) { return `${p.surface} m² ${p.price}k ${Math.round(1000 * p.price / p.surface)} /m²`; }
+
+  let places = [];
+  let selectedPlace = undefined;
+
+  function LeafletTooltipApp(place) {
+    return Vue.createApp({}).component('leaflet-place-tooltip', {
+      data() { return { place: place } },
+      methods: {
+        title() { return placeTitle(this.place); }
+      },
+      // render() { return Vue.h('span', {}, this.title()); }
+      render() { return Vue.createTextVNode(this.title()); }
+      // template: 'plop'
+    });
+  }
+
+  function LeafletPlaceMarkerApp(place) {
+    return Vue.createApp({}).component('leaflet-place-marker', {
+      data() { return { place: place } },
+      methods: {
+        markerColor() {
+          if (this.place.type == 'M') return '#0bc32b';
+          if (this.place.type == 'T2') return '#c30b82';
+          if (this.place.type == 'T3') return '#0b73c3';
+        }
+      },
+      template: `
+      <div :style="{ backgroundColor: markerColor() }" class="marker-pin"></div>
+      <i v-if="place.type === 'M'" class="material-icons" style="color: rgb(0, 0, 0);">house</i>
+      <span v-else-if="place.type === 'T3' || place.type === 'T4'" style="color: rgb(0, 0, 0);">{{ place.type }}</span>
+      `
+    });
   }
 
   let placesApp = Vue.createApp({
     data() {
       return {
-        map: null,
-        popup: L.popup(),
-        places: [],
-        selectedPlace: undefined
+        places: places,
+        selectedPlace: selectedPlace
       }
     },
-    mounted() {
-      this.initMap();
-      this.loadPlaces();
-    },
+    mounted() {},
     methods: {
-      initMap() {
-        var vm = this;
-        this.map = L.map('map', {zoomControl: false}).setView([48.77199820642046, 2.0856456177490124], 15);
-
-        L.control.zoom({position:'topright'}).addTo(this.map);
-
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1
-        }).addTo(this.map);
-
-        this.map.on('click', function(e) {
-          vm.popup
-          .setLatLng(e.latlng)
-          .setContent(e.latlng.toString())
-          .openOn(this);
-        });
-      },
-      title(p) { return p.surface + 'm² ' + p.price + 'k ' + Math.round(1000 * p.price / p.surface) + '/m²'; },
+      title(p) { return placeTitle(p); },
       addPlaceToMap(p) {
         var vm = this;
+
+        p.markerIconApp = LeafletPlaceMarkerApp(p);
+        let markerIconNode = document.createElement('div');
+        {
+          markerIconNode.appendChild(document.createElement('leaflet-place-marker'));
+          p.markerIconApp.mount(markerIconNode);
+        }
+
+        p.markerTooltipApp = LeafletTooltipApp(p);
+        let markerTooltipNode = document.createElement('span');
+        {
+          markerTooltipNode.appendChild(document.createElement('leaflet-place-tooltip'));
+          p.markerTooltipApp.mount(markerTooltipNode);
+        }
+
         p.marker = L.marker([p.lat, p.lon],
           {
             icon: L.divIcon({
               className: 'custom-div-icon',
-              html: entry_icon(p.type),
+              html: markerIconNode,
               iconSize: [30, 42],
               iconAnchor: [15, 42]
             })
-          })
-          .addTo(this.map)
-          .bindTooltip(this.title(p), {permanent: true, direction: 'right', offset: {x: 10, y: -19}, className: 'text-only-tooltip'});
-          p.marker.place = p;
-          p.marker.on('click', function(e) { vm.selectedPlace = e.target.place; });
-      },
-      loadPlaces() {
-        var vm = this;
-        axios({method: 'get', url: '/places', responseType: 'json', headers: {'Accept': 'application/json' }})
-        .then(function (response) {
-          response.data.forEach(e => { e.visible = true; e.selected = false; });
-          vm.places = response.data;
-          vm.places.forEach(vm.addPlaceToMap);
           });
+
+        p.marker.bindTooltip(markerTooltipNode,
+                             {permanent: true, direction: 'right', offset: {x: 10, y: -19}, className: 'text-only-tooltip'});
+
+        p.marker.on('click', function(e) { vm.selectedPlace = p; });
+
+        p.marker.addTo(map);
       },
       placeVisibilityChanged(place) {
         if (place.visible)
-          place.marker.addTo(this.map);
+          place.marker.addTo(map);
         else
           place.marker.remove();
       }
@@ -84,10 +109,19 @@ jQuery(document).ready(function($){
       selectedPlace(curr, prev) {
         if (prev)
           prev.selected = false;
-          curr.selected = true;
-        this.map.setView([curr.lat, curr.lon]);
+        curr.selected = true;
+        map.setView([curr.lat, curr.lon]);
       }
     }
   });
-  var placesVM = placesApp.mount('#places');
+
+  placesVM = placesApp.mount('#places');
+
+  axios({method: 'get', url: '/places', responseType: 'json', headers: {'Accept': 'application/json' }})
+  .then(function (response) {
+    response.data.forEach((p) => {
+      p.visible = true; p.selected = false;
+      placesVM.places.push(p);
+      placesVM.addPlaceToMap(p); });
+  });
 });
